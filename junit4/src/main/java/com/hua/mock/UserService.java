09f6bdb69872c3c9a5134f60799b8d6934964d89
qianye.zheng <>
2021-01-11 17:22:26 +0800

    no message

---
+++ junit4/src/main/java/com/hua/mock/UserService.java
@@ -0,0 +1,29 @@
+/**
+  * @filename UserService.java
+  * @description 
+  * @version 1.0
+  * @author qianye.zheng
+ */
+package com.hua.mock;
+
+import java.time.LocalDate;
+
+/**
+ * @type UserService
+ * @description 
+ * @author qianye.zheng
+ */
+public class UserService {
+ 
+    
+    /**
+     * 
+     * @description 
+     * @return
+     * @author qianye.zheng
+     */
+    public LocalDate getEntryDate() {
+        return LocalDate.now();
+    }
+    
+}
---
+++ powermock/src/main/java/com/hua/mock/UserBusiness.java
@@ -0,0 +1,29 @@
+/**
+  * @filename UserBusiness.java
+  * @description 
+  * @version 1.0
+  * @author qianye.zheng
+ */
+package com.hua.mock;
+
+ 
+/**
+ * @type UserBusiness
+ * @description 
+ * @author qianye.zheng
+ */
+public final class UserBusiness {
+    
+    /**
+     * 
+     * @description 
+     * @return
+     * @author qianye.zheng
+     */
+    public static final int count() {
+        return -1;
+    }
+    
+    
+    
+}
